5ca928c72e95ea642de90e481c6f44ebd10bc995
Chris Clarkson <chris@caxa-technologies.co.uk>
2025-10-02 14:20:10 +0100

    Add graceful shutdown to Server

--- internal/github/client.go
+++ internal/github/client.go
@@ -1,10 +1,15 @@
 package github
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func New(url string) GithubService {
@@ -46,6 +51,25 @@ func (c *GithubClient) GetGists(username string) ([]Gist, error) {
 }
 
 func (c *GithubClient) Serve(port int) {
+	server := &http.Server{
+		Addr: fmt.Sprintf(":%d", port),
+	}
+
+	go func() {
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+		<-sigChan
+
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			fmt.Printf("Error during server shutdown: %v\n", err)
+		} else {
+			fmt.Println("Server gracefully stopped")
+		}
+	}()
+
 	http.HandleFunc("/{user}", func(w http.ResponseWriter, r *http.Request) {
 		user := r.URL.Path[len("/"):]
 		if user == "" {
@@ -62,7 +86,10 @@ func (c *GithubClient) Serve(port int) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(gists)
 	})
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
 }
 
 func unMarshalData(jsondata string) ([]Gist, error) {
